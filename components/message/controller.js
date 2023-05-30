12e8d635a6b2924ef59d5b24a0dd92747ac76b79
Eddy Huaylla Quispe <eddy_huaylla47@hotmail.com>
2023-05-29 19:09:43 -0500

    Add try catch to controller message

--- components/message/controller.js
+++ components/message/controller.js
@@ -2,46 +2,55 @@ const { add, list, update } = require("./store");
 
 const getMessages = () => {
 	return new Promise( (resolve, reject) => {
-		resolve( list() );
+		try {
+			resolve( list() );
+		} catch( error ) {
+			reject( error );
+		}
 	});
 }
 
 const addMessage = ( user, message ) => {
 	return new Promise( (resolve, reject) => {
+		try {
+			user = user.trim()
+			message = message.trim()
 
-		user = user.trim()
-		message = message.trim()
+			if( !user || !message || user === "" || message === "" ) {
+				reject('Los datos son incorrectos');
+			}
 
-		if( !user || !message || user === "" || message === "" ) {
-			reject('Los datos son incorrectos');
+			const full_message = {
+				user : user,
+				message: message,
+				date : new Date()
+			}
+
+			add( full_message );
+
+			resolve( full_message );
+		} catch ( error ) {
+			reject( error );
 		}
-
-		const full_message = {
-			user : user,
-			message: message,
-			date : new Date()
-		}
-
-		add( full_message );
-
-		resolve( full_message );
-	})
+	});
 }
 
 const updateMessage = ( id, text ) => {
 	return new Promise( async ( resolve, reject ) => {
+		try {
+			id = id.trim();
+			text = text.trim();
 
-		id = id.trim();
-		text = text.trim();
+			if( !id || !text || id === "" || text === "" ) {
+				reject( 'Los datos son incorrectos' );
+			}
 
-		if( !id || !text || id === "" || text === "" ) {
-			reject('Los datos son incorrectos');
+			const result = await update( id, text );
+
+			resolve( result );
+		} catch ( error ) {
+			reject( error );
 		}
-
-		const result = await update( id, text );
-
-		resolve( result );
-
 	} );
 }
 
